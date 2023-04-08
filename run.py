622b48cf0421e5ccc1616a7b9c9b6ad23cbf4132
Matthew Mc Daid <107116155+Mcdaid101@users.noreply.github.com>
2023-04-08 20:07:59 +0000

    added bootstrap

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 import os
-from flask import Flask, render_template 
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/contact")
 def contact():
@@ -25,4 +29,4 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+        debug=True)    
